443f20afb28ebaaeb46005030a77b7a5d9e3d366
ArchanaGreddy <46511819+ArchanaGreddy@users.noreply.github.com>
2019-10-04 13:07:24 +0530

    changes

--- src/main/java/com/ihealthpharm/masters/controller/PharmacyController.java
+++ src/main/java/com/ihealthpharm/masters/controller/PharmacyController.java
@@ -2,6 +2,7 @@ package com.ihealthpharm.masters.controller;
 
 import static org.springframework.http.HttpStatus.OK;
 
+import java.io.IOException;
 import java.util.List;
 
 import javax.validation.Valid;
@@ -16,9 +17,12 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ihealthpharm.commons.BaseDto;
 import com.ihealthpharm.masters.helper.PharmacyHelper;
+import com.ihealthpharm.masters.model.HospitalModel;
 import com.ihealthpharm.masters.model.PharmacyModel;
 import com.ihealthpharm.masters.service.PharmacyService;
 
@@ -37,24 +41,40 @@ public class PharmacyController {
 	
 
 	@PostMapping("/save/pharmacy")
-	public ResponseEntity<BaseDto<PharmacyModel>> insertPharmacyData(@Valid @RequestBody PharmacyModel pharmacyModel){
-		log.info("Request Object insert is :",pharmacyModel);
+	public ResponseEntity<BaseDto<PharmacyModel>> insertPharmacyData(@Valid @RequestParam("pharmacyModel") String pharmacyData,@RequestParam("pharmacyLogoPath") MultipartFile pharmacyLogoPath){
+		log.info("Request Object insert is :",pharmacyData);
+		PharmacyModel pharmacyModel = null;
+		try {
+			pharmacyModel = new ObjectMapper().readValue(pharmacyData, PharmacyModel.class);
+			pharmacyModel.setPharmacyLogoPath(pharmacyLogoPath.getBytes());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		PharmacyModel pharmacyModelRes=pharmacyService.savePharmacyData(pharmacyModel);
 		return new BaseDto<>(pharmacyModelRes,pharmacyHelper.getSavePharmacyMessage(),OK).respond();
 	}
 	
 	@PutMapping("/update/pharmacy")
-	public ResponseEntity<BaseDto<PharmacyModel>> updatePharmacyData(@Valid @RequestBody PharmacyModel pharmacyModel){
-		log.info("Request Object for update is: ", pharmacyModel);
+	public ResponseEntity<BaseDto<PharmacyModel>> updatePharmacyData(@Valid @RequestParam("pharmacyModel") String pharmacyData,@RequestParam("pharmacyLogoPath") MultipartFile pharmacyLogoPath){
+		log.info("Request Object for update is: ", pharmacyData);
+		PharmacyModel pharmacyModel = null;
+		try {
+			pharmacyModel = new ObjectMapper().readValue(pharmacyData, PharmacyModel.class);
+			pharmacyModel.setPharmacyLogoPath(pharmacyLogoPath.getBytes());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		PharmacyModel pharmacyModelRes = pharmacyService.updatePharmacyData(pharmacyModel);
 		return new BaseDto<>(pharmacyModelRes,pharmacyHelper.getUpdatePharmacyMessage(),OK).respond();
 	}
 	
 	@PutMapping("/update/pharmacies")
-	public ResponseEntity<BaseDto<List<PharmacyModel>>> updateMultiplePharmaciessData(@Valid @RequestBody List<PharmacyModel> pharmacyModels) {
-		log.info("Request Object for update is: "+ pharmacyModels);
-		List<PharmacyModel> modelRes = pharmacyService.updateMultiplePharmacyData(pharmacyModels);
-		return new BaseDto<>(modelRes,pharmacyHelper.getUpdatePharmacyMessage(),OK).respond();
+	public ResponseEntity<BaseDto<List<PharmacyModel>>> updateHospital(@Valid @RequestBody List<PharmacyModel> pharmacyModel) {
+		log.info("Hospital data update request: ", pharmacyModel);
+		List<PharmacyModel> res=pharmacyService.updateMultiplePharmacyData(pharmacyModel);
+		return new BaseDto<>(res, pharmacyHelper.getUpdatePharmacyMessage(), OK).respond();
 	}
 	
 	
