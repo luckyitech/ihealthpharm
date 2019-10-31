853c52c89ef2f8403b9cea61f0c60038056680b9
jagadeesh <g.jagadeesh.j@gmail.com>
2019-10-31 18:12:18 +0530

    new changes

--- src/main/java/com/ihealthpharm/masters/dao/CustomerInsuranceRepository.java
+++ src/main/java/com/ihealthpharm/masters/dao/CustomerInsuranceRepository.java
@@ -9,4 +9,6 @@ import com.ihealthpharm.masters.model.CustomerInsuranceModel;
 public interface CustomerInsuranceRepository extends JpaRepository<CustomerInsuranceModel,Integer>
 {
 	List<CustomerInsuranceModel> findAll();
+
+	CustomerInsuranceModel findByPolicyCode(String policyCode);
 }
