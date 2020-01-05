03034fe51b6074a8efc0f828792fdda42d4ba363
tarun <tarunmadasu255@gmail.com>
2020-01-05 16:58:20 +0530

    finace java

--- src/main/java/com/ihealthpharm/finance/controller/AccountPayablesController.java
+++ src/main/java/com/ihealthpharm/finance/controller/AccountPayablesController.java
@@ -6,7 +6,6 @@ import java.util.List;
 
 import javax.validation.Valid;
 
-import org.apache.commons.logging.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
