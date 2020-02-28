9a6d4a62b84ea0d89b8e889528db724467a4ceba
unknown <tarunmadasu255@gmail.com>
2020-02-28 10:01:39 +0530

    expenses changes in java

--- src/main/java/com/ihealthpharm/finance/service/ExpensesService.java
+++ src/main/java/com/ihealthpharm/finance/service/ExpensesService.java
@@ -5,6 +5,7 @@ import com.ihealthpharm.finance.model.ExpensesModel;
 public interface ExpensesService {
 
 	ExpensesModel saveExpenses(ExpensesModel expenseModel);
-	
-		
+
+	Integer updatePettyBalance(Integer pettyCashId, Double balance);
+
 	}
