2da25880aba31f1ff3c8f3a81d1f3a34aa60901e
tarun <tarunmadasu255@gmail.com>
2020-03-14 18:36:36 +0530

    removed all junk code

--- src/main/java/com/ihealthpharm/reports/helper/LabelInstructionReceipt.java
+++ src/main/java/com/ihealthpharm/reports/helper/LabelInstructionReceipt.java
@@ -40,19 +40,13 @@ public class LabelInstructionReceipt extends ReportsPDFUtility {
 	@Override
 		public Document generateReport(List<Map<String, Object>> responseList, ReportsMappingModel model, File responseFile,String inputJson) {
 	    Document document = new Document(new RectangleReadOnly(Utilities.millimetersToPoints(80),420));
-	    System.out.println(inputJson);
-System.out.println("...............................................................................");
-System.out.println(responseList);
 		try {
 			PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream(responseFile));
-			//writer.setSpaceCharRatio(PdfWriter.NO_SPACE_CHAR_RATIO);
 			document.setMargins(40,30,0,0);
 			document.setMarginMirroringTopBottom(true);
 			document.open();
 			addHeader(writer, document,model);
 			addLabelDetails(document, model, responseList);
-			//createTable(document, model, responseList);
-			//addTotals(document, model, responseList);
 			addFooter(writer, model,document); 
 
 		} catch (Exception e) {
@@ -74,61 +68,26 @@ System.out.println(responseList);
 		try {
 			header.setTotalWidth(Utilities.millimetersToPoints(80));
 
-			/*
-			 * // add text PdfPCell leftContent = new PdfPCell();
-			 * leftContent.setBorder(Rectangle.NO_BORDER);
-			 */
-
 			PdfPCell centerContent = new PdfPCell();
 			centerContent.setBorder(Rectangle.NO_BORDER);
 			
-			/*
-			 * PdfPCell rightContent = new PdfPCell();
-			 * rightContent.setBorder(Rectangle.NO_BORDER);
-			 */
 			
 			String headerContent = model.getHeaderContent();
 			if(!ObjectUtils.isEmpty(headerContent)) {
 				HeaderFooterContentDto contentDto = (HeaderFooterContentDto) JsonUtility.jsonToObject(headerContent,HeaderFooterContentDto.class);				
-				/*
-				 * for(HeaderFooterContentDetailsDto dto:contentDto.getLeftContent()) {
-				 * leftContent.addElement(new Phrase(dto.getText(),
-				 * FontFactory.getFont(dto.getFontName(), dto.getSize())));
-				 * 
-				 * }
-				 */
 				for(HeaderFooterContentDetailsDto dto:contentDto.getCenterContent()) {		
 					Paragraph phrase=new Paragraph(dto.getText(), FontFactory.getFont(dto.getFontName(), dto.getSize(),Font.BOLD)); 
 					phrase.setAlignment(1); 
 					centerContent.addElement(phrase);					
 				}
 				
-				/*
-				 * for(HeaderFooterContentDetailsDto dto:contentDto.getRightContent()) {
-				 * rightContent.addElement(new Phrase(dto.getText(),
-				 * FontFactory.getFont(dto.getFontName(), dto.getSize())));
-				 * 
-				 * }
-				 */
 				
 			}
-			///header.addCell(leftContent);
 			header.addCell(centerContent);
-			//header.addCell(rightContent);
 			header.setSpacingAfter(10);
-//			
 			PdfPTable title = new PdfPTable(1);
 			title.setTotalWidth(Utilities.millimetersToPoints(80));
-			//title.setWidthPercentage(50);
-//			
 			Font bold = new Font(FontFamily.HELVETICA,7, Font.BOLD);
-//			PdfPCell titleCell = new PdfPCell(new Phrase(model.getTitle(), bold)); 
-//			titleCell.setColspan(3);
-//			titleCell.setHorizontalAlignment(Element.ALIGN_CENTER);
-//			titleCell.setVerticalAlignment(Element.ALIGN_CENTER);
-//			titleCell.setBorder(0);
-//			title.setSpacingAfter(10);
-//			title.addCell(titleCell);
 			
 		
 			PdfPTable finalFeader = new PdfPTable(1);
@@ -150,275 +109,12 @@ System.out.println(responseList);
 	}
 	
 
-	
-//	public void createTable(Document document, ReportsMappingModel model, List<Map<String, Object>> responseList)
-//			throws DocumentException {
-//
-//		String reportHeader = model.getReportHeader();
-//		List<HeaderDto> headerList = JsonUtility.jsonToList(reportHeader, HeaderDto.class);
-//
-//		
-//		PdfPTable table = new PdfPTable(headerList.size());
-//		table.setWidths(new int[] {1,3,1,1,1,1});
-//		table.setTotalWidth(Utilities.millimetersToPoints(80));
-//		table.setLockedWidth(true);
-//		table.setSpacingAfter(10);
-//		PdfPCell cell = null;
-//		for (HeaderDto hearder : headerList) {
-//			Paragraph headerCell = new Paragraph();
-//			headerCell.setFont(headerFont);
-//			headerCell.add(hearder.getDisplayName());
-//			//headerCell.setFont(FontFactory.getFont("Helvetica",7,Font.BOLD));
-//		
-//			cell = new PdfPCell(headerCell);
-//			cell.setHorizontalAlignment(Element.ALIGN_LEFT);
-//			if(!model.isShowVerticalLines())
-//			  cell.setBorder(Rectangle.BOTTOM);
-//		
-//
-//			table.addCell(cell);
-//		}
-//		table.setHeaderRows(1);
-//
-//		// populate Date
-//		if (!ObjectUtils.isEmpty(responseList)) {
-//			for (Map<String, Object> rowData : responseList) {
-//				for (HeaderDto hearder : headerList) {
-//					Object value = rowData.containsKey(hearder.getColumnName()) ? rowData.get(hearder.getColumnName()): "";		
-//					if(ObjectUtils.isEmpty(value) && true) {
-//						
-//						
-//					}
-//					Font bold = new Font(FontFamily.HELVETICA,7,Font.BOLD);
-//					cell = new PdfPCell(new Phrase(String.valueOf(value),bold));
-//					cell.setHorizontalAlignment(Element.ALIGN_LEFT);
-//					if(!model.isShowVerticalLines())
-//					 cell.setBorder(Rectangle.BOTTOM);
-//					
-//					table.addCell(cell);
-//
-//				}
-//			}
-//		}
-//		log.info("table width [{}]",table.getTotalWidth());
-//		document.add(table);
-//
-//	}
-
-//	private void addTotals(Document document, ReportsMappingModel model, List<Map<String, Object>> responseList) throws DocumentException {
-//		PdfPTable table = new PdfPTable(4);
-//		table.setTotalWidth(Utilities.millimetersToPoints(80));
-//		table.setSpacingAfter(0); 
-//		table.getDefaultCell().setBorder(0);
-//		table.setLockedWidth(true);
-//		
-//		System.out.println(responseList);
-//		int totalItems = responseList.size();
-//		double totalAmount = Double.parseDouble(String.valueOf(responseList.get(0).get("TOTAL_AMOUNT")));//responseList.stream().mapToDouble(mapper->Double.parseDouble(mapper.containsKey("TOTAL_AMOUNT")?String.valueOf(mapper.get("TOTAL_AMOUNT")):"0.0")).sum(); 
-//		Double totalQty = Double.parseDouble(String.valueOf(responseList.get(0).get("TOTAL_QTY")));//responseList.stream().mapToDouble(mapper->Double.parseDouble(mapper.containsKey("TOTAL_QTY")?String.valueOf(mapper.get("TOTAL_QTY")):"0.0")).sum(); 
-//		double totalDiscount = Double.parseDouble(String.valueOf(responseList.get(0).get("OVERALL_DISCOUNT")));//responseList.stream().mapToDouble(mapper->Double.parseDouble(mapper.containsKey("OVERALL_DISCOUNT")?String.valueOf(mapper.get("EFFECTIVE_OVERALL_DISCOUNT")):"0.0")).sum(); 
-//		
-//		double totalVat = Double.parseDouble(String.valueOf(responseList.get(0).get("VAT_AMT")));//responseList.stream().mapToDouble(mapper->Double.parseDouble(mapper.containsKey("EFFECTIVE_VAT")?String.valueOf(mapper.get("VAT")):"0.0")).sum();
-//		
-//		//double totalNetAmount = responseList.stream().mapToDouble(mapper->Double.parseDouble(mapper.containsKey("SALE_AMOUNT")?String.valueOf(mapper.get("SALE_AMOUNT")):"0.0")).sum();
-//		double totalNetAmount =Double.parseDouble(String.valueOf(responseList.get(0).get("NET_AMOUNT")));
-//		double balanceAmt =Double.parseDouble(String.valueOf(responseList.get(0).get("BALANCE_AMOUNT")));
-//		double paidAmt =Double.parseDouble(String.valueOf(responseList.get(0).get("PAID_AMOUNT")));
-//		String servBy = (ObjectUtils.isEmpty(responseList))?"":String.valueOf(responseList.get(0).get("FIRST_NM"));
-//		String printedBy = (ObjectUtils.isEmpty(responseList))?"":String.valueOf(responseList.get(0).get("SERVED_BY"));
-//		String paymentStatus = (ObjectUtils.isEmpty(responseList))?"":String.valueOf(responseList.get(0).get("PAYMENT_STATUS"));
-//		String remarks = (ObjectUtils.isEmpty(responseList))?"":String.valueOf(responseList.get(0).get("REMARKS"));
-//		
-//		/*for(Map<String, Object> obj:responseList) {
-//			
-//			
-//			totalVat += (Double.parseDouble(String.valueOf(obj.get("TOTAL_QTY")))*Double.parseDouble(String.valueOf(obj.get("MRP"))))*(1-Double.parseDouble(String.valueOf(obj.get("DISCOUNT_PERCENTAGE")))/100)*
-//					(Double.parseDouble(String.valueOf(obj.get("VAT")))/100);
-//			totalNetAmount +=Double.parseDouble(String.valueOf(obj.get("TOTAL_QTY")))*Double.parseDouble(String.valueOf(obj.get("MRP")));
-//		}
-//		
-//		totalNetAmount += totalVat;
-//		totalNetAmount = Math.round(totalNetAmount - totalDiscount);*/
-//		
-//		Font bold = new Font(FontFamily.HELVETICA,7, Font.BOLD);
-//		PdfPCell cell = new PdfPCell(new Phrase("Tot. Items  :",bold));
-//		cell.setHorizontalAlignment(Element.ALIGN_LEFT);
-//		cell.setBorder(Rectangle.TOP);
-//		table.addCell(cell);
-//		
-//		cell = new PdfPCell(new Phrase(String.valueOf(totalItems),bold));
-//		cell.setHorizontalAlignment(Element.ALIGN_LEFT);
-//		cell.setBorder(Rectangle.TOP);
-//		table.addCell(cell);
-//		
-//		cell = new PdfPCell(new Phrase("Tot. Amt  : ",bold));
-//		cell.setHorizontalAlignment(Element.ALIGN_LEFT);
-//		cell.setBorder(Rectangle.TOP);
-//		table.addCell(cell);
-//		
-//		cell = new PdfPCell(new Phrase(ReportsPDFUtility.decilFormatter.format(totalAmount),bold));
-//		cell.setHorizontalAlignment(Element.ALIGN_LEFT);
-//		cell.setBorder(Rectangle.TOP);
-//		table.addCell(cell);
-//		
-//		cell = new PdfPCell(new Phrase("Tot. Qty.    :",bold));
-//		cell.setHorizontalAlignment(Element.ALIGN_LEFT);
-//		cell.setBorder(Rectangle.NO_BORDER);
-//		table.addCell(cell);
-//		
-//		cell = new PdfPCell(new Phrase(String.valueOf(totalQty.intValue()),bold));
-//		cell.setHorizontalAlignment(Element.ALIGN_LEFT);
-//		cell.setBorder(Rectangle.NO_BORDER);
-//		table.addCell(cell);
-//		
-//		cell = new PdfPCell(new Phrase("Tot. Dis.  : ",bold));
-//		cell.setHorizontalAlignment(Element.ALIGN_LEFT);
-//		cell.setBorder(Rectangle.NO_BORDER);
-//		table.addCell(cell);
-//		
-//		cell = new PdfPCell(new Phrase(ReportsPDFUtility.decilFormatter.format(totalDiscount),bold));
-//		cell.setHorizontalAlignment(Element.ALIGN_LEFT);
-//		cell.setBorder(Rectangle.NO_BORDER);
-//		table.addCell(cell);
-//		
-//		cell = new PdfPCell(new Phrase("Tax Amt    :",bold));
-//		cell.setHorizontalAlignment(Element.ALIGN_LEFT);
-//		cell.setBorder(Rectangle.NO_BORDER);
-//		table.addCell(cell);
-//		
-//		cell = new PdfPCell(new Phrase(ReportsPDFUtility.decilFormatter.format(totalVat),bold));
-//		cell.setHorizontalAlignment(Element.ALIGN_LEFT);
-//		cell.setBorder(Rectangle.NO_BORDER);
-//		table.addCell(cell);
-//
-//		cell = new PdfPCell(new Phrase("Net Amt   :",bold));
-//		cell.setHorizontalAlignment(Element.ALIGN_LEFT);
-//		cell.setBorder(Rectangle.NO_BORDER);
-//		table.addCell(cell);
-//		
-//		cell = new PdfPCell(new Phrase(ReportsPDFUtility.decilFormatter.format(totalNetAmount),bold));
-//		cell.setHorizontalAlignment(Element.ALIGN_LEFT);
-//		cell.setBorder(Rectangle.NO_BORDER);
-//		table.addCell(cell);
-//		
-//		cell = new PdfPCell(new Phrase("Bal Amt    :",bold));
-//		cell.setHorizontalAlignment(Element.ALIGN_LEFT);
-//		cell.setBorder(Rectangle.BOTTOM);
-//		table.addCell(cell);
-//		
-//		cell = new PdfPCell(new Phrase(ReportsPDFUtility.decilFormatter.format(balanceAmt),bold));
-//		cell.setHorizontalAlignment(Element.ALIGN_LEFT);
-//		cell.setBorder(Rectangle.BOTTOM);
-//		table.addCell(cell);
-//		
-//		cell = new PdfPCell(new Phrase("Paid Amt   :",bold));
-//		cell.setHorizontalAlignment(Element.ALIGN_LEFT);
-//		cell.setBorder(Rectangle.BOTTOM);
-//		table.addCell(cell);
-//		
-//		cell = new PdfPCell(new Phrase(ReportsPDFUtility.decilFormatter.format(paidAmt),bold));
-//		cell.setHorizontalAlignment(Element.ALIGN_LEFT);
-//		cell.setBorder(Rectangle.BOTTOM);
-//		table.addCell(cell);
-//		
-//		cell = new PdfPCell(new Phrase("Serv By     :",bold));
-//		cell.setHorizontalAlignment(Element.ALIGN_LEFT);
-//		cell.setBorder(Rectangle.NO_BORDER);
-//		table.addCell(cell);
-//		
-//		cell = new PdfPCell(new Phrase(servBy,bold));
-//		cell.setHorizontalAlignment(Element.ALIGN_LEFT);
-//		cell.setBorder(Rectangle.NO_BORDER);
-//		table.addCell(cell);
-//		
-//		cell = new PdfPCell(new Phrase("Paid Status:",bold));
-//		cell.setHorizontalAlignment(Element.ALIGN_LEFT);
-//		cell.setBorder(Rectangle.NO_BORDER);
-//		table.addCell(cell);
-//		
-//		cell = new PdfPCell(new Phrase(paymentStatus,bold));
-//		cell.setHorizontalAlignment(Element.ALIGN_LEFT);
-//		cell.setBorder(Rectangle.NO_BORDER);
-//		table.addCell(cell);
-//		
-//		cell = new PdfPCell(new Phrase("Print By     :",bold));
-//		cell.setHorizontalAlignment(Element.ALIGN_LEFT);
-//		cell.setBorder(Rectangle.BOTTOM);
-//		table.addCell(cell);
-//		
-//		cell = new PdfPCell(new Phrase(printedBy ,bold));
-//		cell.setHorizontalAlignment(Element.ALIGN_LEFT);
-//		cell.setBorder(Rectangle.BOTTOM);
-//		table.addCell(cell);
-//		
-//		
-//		cell = new PdfPCell(new Phrase("     "));
-//	    cell.setHorizontalAlignment(Element.ALIGN_LEFT);
-//		cell.setBorder(Rectangle.BOTTOM);
-//		table.addCell(cell);
-//	
-//		cell = new PdfPCell(new Phrase("     "));
-//		cell.setHorizontalAlignment(Element.ALIGN_LEFT);
-//		cell.setBorder(Rectangle.BOTTOM);
-//		table.addCell(cell);
-//	
-//		
-//		cell = new PdfPCell(new Phrase("Tax Codes:",bold));
-//		cell.setHorizontalAlignment(Element.ALIGN_LEFT);
-//		cell.setBorder(Rectangle.BOTTOM);
-//		table.addCell(cell);
-//		
-//		cell = new PdfPCell(new Phrase("A - 16%:",bold));
-//		cell.setHorizontalAlignment(Element.ALIGN_LEFT);
-//		cell.setBorder(Rectangle.BOTTOM);
-//		table.addCell(cell);
-//		
-//		cell = new PdfPCell(new Phrase("B - 0%:",bold));
-//		cell.setHorizontalAlignment(Element.ALIGN_LEFT);
-//		cell.setBorder(Rectangle.BOTTOM);
-//		table.addCell(cell);
-//		
-//		cell = new PdfPCell(new Phrase("E - Exempt",bold));
-//		cell.setHorizontalAlignment(Element.ALIGN_LEFT);
-//		cell.setBorder(Rectangle.BOTTOM);
-//		table.addCell(cell);
-//		
-//		table.completeRow();
-//		
-//		document.add(table);
-//		
-//		PdfPTable table_remarks = new PdfPTable(1);
-//		table_remarks.setTotalWidth(Utilities.millimetersToPoints(80));
-//		table_remarks.getDefaultCell().setBorder(0);
-//		table_remarks.setLockedWidth(true);
-//		
-//		cell = new PdfPCell(new Phrase("Remarks   :  " + remarks ,bold));
-//		cell.setHorizontalAlignment(Element.ALIGN_LEFT);
-//		cell.setBorder(Rectangle.BOTTOM);
-//		table_remarks.addCell(cell);
-//		
-//		table_remarks.completeRow();
-//		
-//		document.add(table_remarks);
-//	}
-
 	private void addLabelDetails(Document document, ReportsMappingModel model, List<Map<String, Object>> responseList) throws DocumentException {
 		
 		
 		DateFormat f = new SimpleDateFormat("dd/MM/yyyy");
 		String expDate="";
 		 Date date = new Date();
-		
-//		if(responseList.get(0).get("LAST_UPDATE_TS")==null) {
-//			
-//		}else {
-//			billDate = f.format((ObjectUtils.isEmpty(responseList))?"":responseList.get(0).get("LAST_UPDATE_TS"));
-//		}
-		//String billDate = f.format((ObjectUtils.isEmpty(responseList))?"":responseList.get(0).get("LAST_UPDATE_TS"));
-		 System.out.println("*********************************");
-		 System.out.println(responseList.size());
-		System.out.println(responseList);
-		 System.out.println("*********************************");
 		
 		String itemName = (ObjectUtils.isEmpty(responseList))?"":String.valueOf(responseList.get(0).get("ITEM_NM"));
 		String quantity= (ObjectUtils.isEmpty(responseList))?"":String.valueOf(responseList.get(0).get("ITEM_QTY"));
@@ -498,28 +194,6 @@ System.out.println(responseList);
 		table.setLockedWidth(true);
 		table.getDefaultCell().setBorder(0);
 		table.setWidths(new int[] {1,3,2,2});
-//		
-//		Font bolds = new Font(FontFamily.HELVETICA,7, Font.BOLD);
-//		PdfPCell cell = new PdfPCell(new Phrase(" I : ",bold));
-//		cell.setHorizontalAlignment(Element.ALIGN_LEFT);
-//		cell.setBorder(Rectangle.TOP);
-//		table.addCell(cell);
-		
-	
-	
-	
-//		
-//		PdfPCell  cell = new PdfPCell(new Phrase("Item  :",bold));
-//		cell.setHorizontalAlignment(Element.ALIGN_LEFT);
-//		cell.setBorder(Rectangle.TOP);
-//		table.addCell(cell);
-//		
-//		cell = new PdfPCell(new Phrase(itemName,bold));
-//		cell.setHorizontalAlignment(Element.ALIGN_LEFT);
-//		cell.setBorder(Rectangle.TOP);
-//		table.addCell(cell);
-		
-
 		
 		PdfPCell cell = new PdfPCell(new Phrase("Qty    : ",bold));
 		cell.setHorizontalAlignment(Element.ALIGN_LEFT);
@@ -563,16 +237,6 @@ System.out.println(responseList);
 		cell.setBorder(Rectangle.BOTTOM);
 		table.addCell(cell);
 		
-//		cell = new PdfPCell(new Phrase());
-//		cell.setHorizontalAlignment(Element.ALIGN_LEFT);
-//		cell.setBorder(Rectangle.BOTTOM);
-//		table.addCell(cell); 
-//		
-//		cell = new PdfPCell(new Phrase());
-//		cell.setHorizontalAlignment(Element.ALIGN_LEFT);
-//		cell.setBorder(Rectangle.BOTTOM);
-	
-//		table.addCell(cell); 
 		table.completeRow();
 		
 		document.add(table);
